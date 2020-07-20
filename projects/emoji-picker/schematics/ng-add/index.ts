73c5fdf21bc5173fbe9955696021c49c1baa85ac
e.khasanova <e.khasanova@rambler-co.ru>
2020-07-20 18:15:19 +0300

    added schematics

---
+++ projects/emoji-picker/schematics/ng-add/index.ts
@@ -0,0 +1,10 @@
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+
+
+export function ngAdd(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    _context.addTask(new NodePackageInstallTask());
+    return tree;
+  };
+}
