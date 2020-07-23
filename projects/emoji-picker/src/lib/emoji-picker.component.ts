2d4df4eba8d4fdc5afc47b8321bbb52cdfaf345d
sphere89 <tolarionov@gmail.com>
2020-07-23 15:07:53 +0300

    Add fixes

--- projects/emoji-picker/src/lib/emoji-picker.component.ts
+++ projects/emoji-picker/src/lib/emoji-picker.component.ts
@@ -14,12 +14,21 @@ export class EmojiPickerComponent<T extends EmojiMap> implements OnInit {
   @Input() source: EmojiSourceFn<T>;
   @Input() activeGroup?: string;
   @Output() onSelect: EventEmitter<EmojiData> = new EventEmitter<EmojiData>();
+  // TODO Контейнер лучше передавать в компонент
   @ViewChild('container') container: ElementRef;
 
   constructor(private ngZone: NgZone) { }
 
   ngOnInit() {
     this.ngZone.runOutsideAngular(() => {
+      // 1 - Инициализацию надо делать - но не рендерить
+      // 2 - ссылку на EmojiPicker надо сохранить как поле компонента
+      // чтобы можно было вызывать его методы
+      // Например, передали контейнер, навесили клик на кнопку
+      // В обработчике кнопки, вызвали поповер получили ссылку на пикер отренедерили там виджет
+
+      // В итоге надо все это упоковать в библиотеку - чтобы она сама вызывала поповер, а в проект мы просто добавляли
+      // Компонент который рендерит кнопку, по клику на которую в CDK поповере рендерится виджет
       const emojiBlock = new EmojiPicker(this.container.nativeElement, {
         source: this.source,
         defaultActiveGroup: this.activeGroup,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// TODO CHANGE TO npm package name
+import { EmojiPickerModule } from 'projects/emoji-picker/src/lib/emoji-picker.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EmojiPickerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
